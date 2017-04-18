6760a44e2110e5376b395588b422f8f70d01fe63
laurens de vries <leh.devries@gmail.com>
2017-04-18 14:08:43 +0200

    finished it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "Do laundry", details: "go to laundryhouse, dry that shit", done: "false")
+Task.create(name: "Get food", details: "got to the store", done: "false")
+Task.create(name: "Find dog", details: "dog went to the park, find him there", done: "false")
+Task.create(name: "Do laundry", details: "go to laundryhouse, dry that shit", done: "false")
+Task.create(name: "Get food", details: "got to the store", done: "false")
+Task.create(name: "Find dog", details: "dog went to the park, find him there", done: "false")
